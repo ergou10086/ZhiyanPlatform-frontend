632dda737008eec0393252dbb8c5b65809c64aed
霍思翰 <1977127178@qq.com>
2025-10-15 17:27:51 +0800

    知识库其余页面添加，项目广场修改

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -20,7 +20,7 @@
           </div>
           <span class="nav-text">首页</span>
         </div>
-        
+
         <div class="nav-item" :class="{ active: currentRoute === '/project-square' }" @click="navigateTo('/project-square')">
           <div class="nav-icon">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
