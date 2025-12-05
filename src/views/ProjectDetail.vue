fb8f0e6150bc25f137aed8c632c19050fa36e8ff
pxlin <768331153@qq.com>
2025-12-05 09:17:34 +0800

    搜索用户头像

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -999,7 +999,7 @@
                 @click="selectUser(user)"
               >
                 <div class="user-avatar">
-                  <img v-if="user.avatarUrl || user.avatar" :src="user.avatarUrl || user.avatar" alt="用户头像" />
+                  <img v-if="user.avatarUrl || user.avatar || user.avatarData" :src="user.avatarUrl || user.avatar || user.avatarData" alt="用户头像" />
                   <div v-else class="avatar-placeholder">
                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M20 21V19C20 17.9391 19.5786 16.9217 18.8284 16.1716C18.0783 15.4214 17.0609 15 16 15H8C6.93913 15 5.92172 15.4214 5.17157 16.1716C4.42143 16.9217 4 17.9391 4 19V21" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
