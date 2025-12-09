6d6ce48d49e4cb4ae2ba761dcf3d4550161ceb89
ErguTree <zjm88822201@126.com>
2025-12-09 10:23:06 +0800

    页脚编辑

--- docs/docs/.vuepress/.temp/internal/siteData.js
+++ docs/docs/.vuepress/.temp/internal/siteData.js
@@ -1,4 +1,4 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"ZhiyanPlatform Doc\",\"description\":\"智研项目平台的使用帮助文档\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"智研平台帮助文档\",\"description\":\"高校科研团队协作与成果管理平台 - 帮助文档\",\"head\":[],\"locales\":{}}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
--- docs/docs/.vuepress/.temp/pages/get-started.html.vue
+++ docs/docs/.vuepress/.temp/pages/get-started.html.vue
@@ -1,23 +1,39 @@
-<template><div><h1 id="get-started" tabindex="-1"><a class="header-anchor" href="#get-started"><span>Get Started</span></a></h1>
-<p>This is a normal page, which contains VuePress basics.</p>
-<h2 id="pages" tabindex="-1"><a class="header-anchor" href="#pages"><span>Pages</span></a></h2>
-<p>You can add markdown files in your vuepress directory, every markdown file will be converted to a page in your site.</p>
-<p>See <a href="https://vuejs.press/guide/page.html#routing" target="_blank" rel="noopener noreferrer">routing</a> for more details.</p>
-<h2 id="content" tabindex="-1"><a class="header-anchor" href="#content"><span>Content</span></a></h2>
-<p>Every markdown file <a href="https://vuejs.press/guide/page.html#content" target="_blank" rel="noopener noreferrer">will be rendered to HTML, then converted to a Vue SFC</a>.</p>
-<p>VuePress support basic markdown syntax and <a href="https://vuejs.press/guide/markdown.html#syntax-extensions" target="_blank" rel="noopener noreferrer">some extensions</a>, you can also <a href="https://vuejs.press/guide/markdown.html#using-vue-in-markdown" target="_blank" rel="noopener noreferrer">use Vue features</a> in it.</p>
-<h2 id="configuration" tabindex="-1"><a class="header-anchor" href="#configuration"><span>Configuration</span></a></h2>
-<p>VuePress use a <code v-pre>.vuepress/config.js</code>(or .ts) file as <a href="https://vuejs.press/guide/configuration.html#client-config-file" target="_blank" rel="noopener noreferrer">site configuration</a>, you can use it to config your site.</p>
-<p>For <a href="https://vuejs.press/guide/configuration.html#client-config-file" target="_blank" rel="noopener noreferrer">client side configuration</a>, you can create <code v-pre>.vuepress/client.js</code>(or .ts).</p>
-<p>Meanwhile, you can also add configuration per page with <a href="https://vuejs.press/guide/page.html#frontmatter" target="_blank" rel="noopener noreferrer">frontmatter</a>.</p>
-<h2 id="layouts-and-customization" tabindex="-1"><a class="header-anchor" href="#layouts-and-customization"><span>Layouts and customization</span></a></h2>
-<p>Here are common configuration controlling layout of <code v-pre>@vuepress/theme-default</code>:</p>
-<ul>
-<li><a href="https://vuejs.press/reference/default-theme/config.html#navbar" target="_blank" rel="noopener noreferrer">navbar</a></li>
-<li><a href="https://vuejs.press/reference/default-theme/config.html#sidebar" target="_blank" rel="noopener noreferrer">sidebar</a></li>
-</ul>
-<p>Check <a href="https://vuejs.press/reference/default-theme/" target="_blank" rel="noopener noreferrer">default theme docs</a> for full reference.</p>
-<p>You can <a href="https://vuejs.press/reference/default-theme/styles.html#style-file" target="_blank" rel="noopener noreferrer">add extra style</a> with <code v-pre>.vuepress/styles/index.scss</code> file.</p>
+<template><div><h1 id="快速开始" tabindex="-1"><a class="header-anchor" href="#快速开始"><span>快速开始</span></a></h1>
+<p>欢迎使用智研平台！本指南将帮助您快速了解和使用平台的核心功能</p>
+<h2 id="平台简介" tabindex="-1"><a class="header-anchor" href="#平台简介"><span>平台简介</span></a></h2>
+<p>智研平台是一个专为高校科研团队设计的协作与成果管理平台，旨在通过技术手段提升科研创新效率，让知识的生产、沉淀与传承更加高效、有序。</p>
+<h2 id="开始使用平台" tabindex="-1"><a class="header-anchor" href="#开始使用平台"><span>开始使用平台</span></a></h2>
+<h3 id="注册" tabindex="-1"><a class="header-anchor" href="#注册"><span>注册</span></a></h3>
+<p>Zhiyan项目平台的注册需要使用你的邮箱来进行注册</p>
+<p><img src="@source/get-started/image-20251209100343146.png" alt="image-20251209100343146"></p>
+<p>请注意，密码强度的设置，平台的密码最少需要七位，而且携带至少两种字符</p>
+<p>其中所属机构与学校字段建议填写真实信息，方便日后统一组织和管理</p>
+<h3 id="登录" tabindex="-1"><a class="header-anchor" href="#登录"><span>登录</span></a></h3>
+<h4 id="第三方登录" tabindex="-1"><a class="header-anchor" href="#第三方登录"><span>第三方登录</span></a></h4>
+<p>GitHub的第三方登录仅仅会获取你的邮箱，头像和昵称都不是强制获取，如果你没有开启Github邮箱的访问权限，第三方登录很可能失败</p>
+<p>只要重定向过一次Github登录，之后就会自动登陆成功</p>
+<p><img src="@source/get-started/image-20251209100407756.png" alt="image-20251209100407756"></p>
+<h4 id="_2fa相关" tabindex="-1"><a class="header-anchor" href="#_2fa相关"><span>2FA相关</span></a></h4>
+<p>强烈建议每位用户在完成注册的第一次登录之后就开启2FA</p>
+<p>否则极有可能引发账号的安全问题，而且团队有考虑在日后的更新中强制使用2FA双重因素认证</p>
+<p>在个人信息页面开启2FA双重因素认证，推荐使用Microsoft Authenticator</p>
+<p><img src="@source/get-started/image-20251209100414464.png" alt="image-20251209100414464"></p>
+<h4 id="安全检查与提示" tabindex="-1"><a class="header-anchor" href="#安全检查与提示"><span>安全检查与提示</span></a></h4>
+<p>登录会进行一次安全检测，如果两次登录IP出现不一致的问题，会给你发送系统消息，需要你进行认证，目前IP定位会出现运营商不一致的问题</p>
+<p>如果出现了上述问题，忽视即可，但是不要忽视其他的系统发来的安全信息</p>
+<p><img src="@source/get-started/image-20251209100444399.png" alt="image-20251209100444399"></p>
+<p>可能出现他人冒充系统账号发送安全信息进行诈骗等安全隐患内容，请注意分辨</p>
+<p><img src="@source/get-started/image-20251209100457273.png" alt="image-20251209100457273"></p>
+<h4 id="登录后主页相关" tabindex="-1"><a class="header-anchor" href="#登录后主页相关"><span>登录后主页相关</span></a></h4>
+<p>登录完成后就会直接进入主页，快捷操作对应项目位置的导航，主页内部展示用户参与的项目，工作项展示未完成的任务，而且主页的展示包括待审核的任务和各种提醒，包括任务提醒和消息提醒</p>
+<p><img src="@source/get-started/image-20251209100522898.png" alt="image-20251209100522898"></p>
+<p>关于注意，如果没有登录，是无法进入除了项目广场的其他浏览位置的，而且游客没有任何除了查看部分公开资源的其他任何权限，实际上，项目是一个强登录需求的项目</p>
+<h2 id="个人信息" tabindex="-1"><a class="header-anchor" href="#个人信息"><span>个人信息</span></a></h2>
+<h3 id="个人信息修改" tabindex="-1"><a class="header-anchor" href="#个人信息修改"><span>个人信息修改</span></a></h3>
+<p>个人信息页面需要登录才能进入并且访问</p>
+<p><img src="@source/get-started/image-20251209101838327.png" alt="image-20251209101838327"></p>
+<p>总体浏览，个人信息页面如上，其中大部分信息都可以修改</p>
+<h3 id="" tabindex="-1"><a class="header-anchor" href="#"><span></span></a></h3>
 </div></template>
 
 
