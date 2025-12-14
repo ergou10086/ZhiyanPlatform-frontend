ef5f0801a7871a0f3cea1f253133c20c52f4c70e
ErguTree <zjm88822201@126.com>
2025-12-14 20:08:57 +0800

    探索一些运维上的内容

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -33,9 +33,9 @@
           <div class="footer-section">
             <h3 class="section-title">文档资源</h3>
             <ul class="link-list">
-              <li><a href="http://localhost:8012/docs/" target="_blank" class="footer-link">帮助文档</a></li>
-              <li><a href="http://localhost:8012/docs/get-started.html" target="_blank" class="footer-link">快速开始</a></li>
-              <li><a href="http://localhost:8012/docs/API.html" target="_blank" class="footer-link">API文档</a></li>
+              <li><a :href="docsBaseUrl" class="footer-link">帮助文档</a></li>
+              <li><a :href="docsBaseUrl + 'get-started.html'" class="footer-link">快速开始</a></li>
+              <li><a :href="docsBaseUrl + 'API.html'" class="footer-link">API文档</a></li>
               <li><a href="#" class="footer-link">使用指南</a></li>
             </ul>
           </div>
@@ -116,7 +116,13 @@
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
+  computed: {
+    docsBaseUrl() {
+      // 使用配置的文档基础 URL，默认为 '/docs/'
+      return DOCS_BASE_URL || '/docs/'
+    }
+  }
 }
 </script>
 
