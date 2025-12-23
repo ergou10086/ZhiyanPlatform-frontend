7aa23416aa0ddbe284e1f62188fc5b96ba41b5dd
ErguTree <zjm88822201@126.com>
2025-12-23 11:16:03 +0800

    定期合并

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,6 @@ import ProjectKnowledge from '../views/ProjectKnowledge.vue'
 import ProjectDashboard from '../views/ProjectDashboard.vue'
 import ProjectOperationLog from '../views/ProjectOperationLog.vue'
 import MyActivity from '../views/MyActivity.vue'
-import OAuth2Callback from '../views/OAuth2Callback.vue'
 import OAuth2Error from '../views/OAuth2Error.vue'
 import ChangeEmail from '../views/ChangeEmail.vue'
 import NotFound from '../views/NotFound.vue'
@@ -48,21 +47,6 @@ const routes = [
     path: '/change-email',
     name: 'ChangeEmail',
     component: ChangeEmail
-  },
-  {
-    path: '/auth/oauth2/callback/:provider',
-    name: 'OAuth2Callback',
-    component: OAuth2Callback
-  },
-  {
-    path: '/zhiyan/auth/oauth2/callback/:provider',
-    name: 'OAuth2CallbackWithPrefix',
-    component: OAuth2Callback
-  },
-  {
-    path: '/oauth2/callback',
-    name: 'OAuth2CallbackShort',
-    component: OAuth2Callback
   },
   {
     path: '/oauth2/error',
